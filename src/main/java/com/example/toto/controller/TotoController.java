355c837351e2c9994fb7567e0082d4aa21a6fbc6
Sujin1517 <11tnwls12@gmail.com>
2024-06-14 11:46:22 +0900

    [feat]: Add game Controller mapping(#10)

--- src/main/java/com/example/toto/controller/TotoController.java
+++ src/main/java/com/example/toto/controller/TotoController.java
@@ -1,12 +1,41 @@
 package com.example.toto.controller;
 
+import com.example.toto.domain.dto.request.GameRequest;
+import com.example.toto.domain.dto.request.GameUpdateRequest;
+import com.example.toto.domain.dto.response.GameResponse;
 import com.example.toto.service.TotoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/toto")
 @RequiredArgsConstructor
 public class TotoController {
     private TotoService totoService;
+
+    @GetMapping("/games")
+    public List<GameResponse> getGames(
+            @RequestParam(name = "date", required = false) Date date,
+            @RequestParam(name = "team", required = false) String team
+    ) {
+        return null;
+    }
+
+    @PostMapping("/games")
+    public void addGame(@RequestBody GameRequest req) {
+
+    }
+
+    @PutMapping("/games")
+    public void updateGameResult(@RequestBody List<GameUpdateRequest> req) {
+
+    }
+
+    @DeleteMapping("/games/{id}")
+    public void deleteGame(@RequestParam("id") Long id) {
+
+    }
 }
---
+++ src/main/java/com/example/toto/domain/dto/request/GameUpdateRequest.java
@@ -0,0 +1,4 @@
+package com.example.toto.domain.dto.request;
+
+public record GameUpdateRequest() {
+}
