d511596f8117a4000642cb0851e0d87a662d7f5a
Sujin1517 <11tnwls12@gmail.com>
2024-06-14 22:32:44 +0900

    [feat]: Add NotFoundException(#13)

---
+++ src/main/java/com/example/toto/controller/ExceptionController.java
@@ -0,0 +1,17 @@
+package com.example.toto.controller;
+
+import com.example.toto.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String notFoundException(NotFoundException e) {
+        return e.getMessage() + " IS NOT FOUND";
+    }
+}
