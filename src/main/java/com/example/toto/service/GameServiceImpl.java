0e021a396066ac64c6419b99d896d521f05abf44
Sujin1517 <11tnwls12@gmail.com>
2024-06-15 10:44:59 +0900

    [feat]: Add Result Value Validation(#13)

--- src/main/java/com/example/toto/service/GameServiceImpl.java
+++ src/main/java/com/example/toto/service/GameServiceImpl.java
@@ -7,6 +7,7 @@ import com.example.toto.domain.entity.Game;
 import com.example.toto.domain.repository.GameRepository;
 import com.example.toto.domain.repository.TeamRepository;
 import com.example.toto.exception.NotFoundException;
+import com.example.toto.utils.ResultValidationUtils;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,7 @@ import java.util.List;
 public class GameServiceImpl implements GameService {
     private final GameRepository gameRepository;
     private final TeamRepository teamRepository;
+    private final ResultValidationUtils resultValidationUtils;
 
     @Override
     public List<GameResponse> getGamesByParam(LocalDate date, Long team) {
@@ -44,6 +46,7 @@ public class GameServiceImpl implements GameService {
     public void updateGameResult(List<GameUpdateRequest> req) {
         req.forEach(e -> {
             Game game = gameRepository.findById(e.gameId()).orElseThrow(() -> new NotFoundException("GAME"));
+            resultValidationUtils.gameResultValidation(e.result());
             game.updateResult(e.result());
         });
     }
