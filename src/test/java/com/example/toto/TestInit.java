3afd35faaa71990d2ad5cf783c5aa1c758b09c8b
Sujin1517 <11tnwls12@gmail.com>
2024-06-15 15:42:43 +0900

    [feat]: Add TestInit class(#15) init teamA,B,C,D and game1,2

---
+++ src/test/java/com/example/toto/TestInit.java
@@ -0,0 +1,47 @@
+package com.example.toto;
+
+import com.example.toto.domain.entity.Game;
+import com.example.toto.domain.entity.Team;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.UUID;
+
+public class TestInit {
+    public final Team teamA = new Team(1L, "teamA", null, null);
+    public final Team teamB = new Team(2L, "teamB", null, null);
+    public final Team teamC = new Team(3L, "teamC", null, null);
+    public final Team teamD = new Team(4L, "teamD", null, null);
+    public final Game game1 = new Game(
+            1L,
+            LocalDateTime.of(2024,6,15,18,30),
+            LocalDateTime.of(2024,6,15,18,20),
+            teamA,
+            teamB,
+            1.5f,
+            2f,
+            0
+    );
+    public final Game game2 = new Game(
+            2L,
+            LocalDateTime.of(2024,6,15,18,30),
+            LocalDateTime.of(2024,6,15,18,20),
+            teamC,
+            teamD,
+            1.1f,
+            3f,
+            0
+    );
+
+    private final SecretKey secretKey = Keys.hmacShaKeyFor("SecretKeyHereSecretKeyHereSecretKeyHereSecretKeyHere".getBytes());
+    public final String generateToken(UUID uuid, Integer expiration) {
+        return Jwts.builder()
+                .subject(uuid.toString())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(secretKey)
+                .compact();
+    }
+}
