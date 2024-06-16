73004a968eb68d294ccaf70356221033d3ec82b4
Sujin1517 <11tnwls12@gmail.com>
2024-06-16 11:25:06 +0900

    [feat]: Add basic GameServiceImplTest from(#10)

---
+++ src/test/java/com/example/toto/service/GameServiceImplTest.java
@@ -0,0 +1,76 @@
+package com.example.toto.service;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameServiceImplTest {
+
+    @Nested
+    class getGamesByParam {
+        @Test
+        void 성공_파라미터_모두_있음() {
+
+        }
+
+        @Test
+        void 성공_파라미터_모두_없음() {
+
+        }
+
+        @Test
+        void 성공_파라미터_팀_없음() {
+
+        }
+
+        @Test
+        void 성공_파라미터_날짜_없음() {
+
+        }
+    }
+
+    @Nested
+    class insertGame{
+        @Test
+        void 성공_추가됨() {
+
+        }
+
+        @Test
+        void 실패_팀_없음() {
+
+        }
+    }
+
+    @Nested
+    class updateGameResult {
+        @Test
+        void 성공_갱신됨() {
+
+        }
+
+        @Test
+        void 실패_게임_없음() {
+
+        }
+
+        @Test
+        void 실패_값_검증_실패() {
+
+        }
+    }
+
+    @Nested
+    class deleteGame{
+        @Test
+        void 성공_삭제됨() {
+
+        }
+
+        @Test
+        void 실패_게임_없음() {
+
+        }
+    }
+}
