9077545cbda090fd52af2e824ddaf7f3fa616bd0
Sujin1517 <11tnwls12@gmail.com>
2024-06-18 17:23:14 +0900

    [feat]: Add create simulation game scheduler(#30)

--- src/main/java/com/example/toto/controller/TotoController.java
+++ src/main/java/com/example/toto/controller/TotoController.java
@@ -1,16 +1,20 @@
 package com.example.toto.controller;
 
+import com.example.toto.domain.dto.request.GameRequest;
 import com.example.toto.domain.dto.request.GameUpdateRequest;
 import com.example.toto.service.BettingService;
 import com.example.toto.service.GameService;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.*;
 
+@EnableScheduling
 @RestController
 @RequestMapping("api/v1/toto")
 @RequiredArgsConstructor
@@ -31,5 +35,31 @@ public class TotoController {
     public void setResultGame(@RequestBody GameUpdateRequest request) {
         gameService.updateGameResult(List.of(request));
         bettingService.updateBettingResult(request);
+    }
+
+//    @GetMapping("/set")
+    @Scheduled(cron = "0 0/5 * * * *")
+    public void createGames() {
+        Random rand = new Random();
+        List<GameRequest> gameList = new ArrayList<>();
+        Map<Integer, Integer> teamOrder = new HashMap<>();
+        while(teamOrder.size() < 6) {
+            teamOrder.putIfAbsent(rand.nextInt(6), teamOrder.size()+1);
+        }
+
+        for (int i=0; i<3; i++) {
+            float rtp = rand.nextFloat(1);
+            float value = (float) Math.floor((Math.abs(rtp * 2 - 1) * 3 + 1) * 100) / 100;
+            gameList.add(new GameRequest(
+                    LocalDateTime.now().withSecond(0).withNano(0).plusMinutes(5),
+                    LocalDateTime.now().withSecond(0).withNano(0).plusMinutes(4),
+                    (long) teamOrder.get(i*2),
+                    (long) teamOrder.get(i*2+1),
+                    (float) 1 + value * rtp,
+                    (float) 1 + value * (1-rtp)
+            ));
+        }
+
+        gameService.insertGame(gameList);
     }
 }
