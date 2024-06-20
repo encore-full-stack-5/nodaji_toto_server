28574a3a8f2e29f832c8c14f91ee79b5bb2cabae
Sujin1517 <11tnwls12@gmail.com>
2024-06-20 14:04:01 +0900

    [fix]: Apply yml address in CORS config(#34)

--- src/main/java/com/example/toto/config/WebConfig.java
+++ src/main/java/com/example/toto/config/WebConfig.java
@@ -1,16 +1,27 @@
 package com.example.toto.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private final String frontAddr;
+    private final String paymentAddr;
+
+    public WebConfig(
+            @Value("${address.front}") String frontAddr,
+            @Value("${address.payment}") String paymentAddr
+    ) {
+        this.frontAddr = frontAddr;
+        this.paymentAddr = paymentAddr;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:8080", frontAddr, paymentAddr)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization")
                 .exposedHeaders("Custom-Header")
