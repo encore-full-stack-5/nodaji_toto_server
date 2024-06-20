96ecd99804f85122799df70fd9a45ed05f883828
Sujin1517 <11tnwls12@gmail.com>
2024-06-20 16:40:09 +0900

    [add]: Add thread pool config(#34)

---
+++ src/main/java/com/example/toto/config/AsyncConfig.java
@@ -0,0 +1,20 @@
+package com.example.toto.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+    @Bean
+    ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(100);
+        executor.initialize();
+        return executor;
+    }
+}
