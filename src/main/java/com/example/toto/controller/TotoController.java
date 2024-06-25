2bf76f6f54b681ccc3af1fec90970210516dda17
Sujin1517 <11tnwls12@gmail.com>
2024-06-25 11:51:28 +0900

    [refact]: Move simulation code to local(#34)

--- src/main/java/com/example/toto/controller/TotoController.java
+++ src/main/java/com/example/toto/controller/TotoController.java
@@ -19,62 +19,8 @@ import java.util.*;
 @RequestMapping("api/v1/toto")
 @RequiredArgsConstructor
 public class TotoController {
-    private final BettingService bettingService;
-    private final GameService gameService;
-    private final GameRepository gameRepository; // for test simulation
-
-    @PutMapping("/set-result")
-    public void setResultGame(@RequestBody GameUpdateRequest request) {
-        gameService.updateGameResult(List.of(request));
-        bettingService.updateBettingResult(request);
-    }
-
-    // test simulation method
-    @GetMapping("/set1")
-//    @Scheduled(cron = "0 0/2 * * * *")
-    public void createGames() {
-        int teamNum = 14;
-        Random rand = new Random();
-        List<GameRequest> gameList = new ArrayList<>();
-        Map<Integer, Integer> teamOrder = new HashMap<>();
-        while(teamOrder.size() < teamNum) {
-            teamOrder.putIfAbsent(rand.nextInt(teamNum), teamOrder.size()+1);
-        }
-
-        for (int i=0; i<teamNum/2; i++) {
-            float rtp = rand.nextFloat(0.8f)+0.1f;
-            gameList.add(new GameRequest(
-                    LocalDateTime.now().withSecond(0).withNano(0).plusMinutes(2),
-                    LocalDateTime.now().withSecond(0).withNano(0).plusMinutes(1),
-                    (long) teamOrder.get(i*2),
-                    (long) teamOrder.get(i*2+1),
-                    (float) Math.ceil(Math.pow((rtp * 1.6), 3) * 100) / 100 + 1,
-                    (float) Math.ceil(Math.pow(((1-rtp) * 1.6), 3) * 100) / 100 + 1
-            ));
-        }
-
-        gameService.insertGame(gameList);
-    }
-
-    // test simulation method
-    @GetMapping("/set2")
-//    @Scheduled(cron = "0 0/2 * * * *")
-    public void resultGames() {
-        List<Game> allGamesByResult = gameRepository.findAllGamesByGameResultAndGameStartAtBefore(0, LocalDateTime.now());
-        allGamesByResult.forEach(e -> {
-                    double rand = new Random().nextDouble(1.1);
-                    int result; // 1:홈팀승, 2:원정팀승, 3:무승부, 4:경기취소
-                    if(rand > 1.07) {
-                        result = 4;
-                    } else if(rand > 1) {
-                        result = 3;
-                    } else {
-                        result = Math.cbrt(e.getRtpHome() - 1) / 1.6 < rand ? 1 : 2;
-                    }
-                    GameUpdateRequest gameUpdateRequest = new GameUpdateRequest(e.getGameId(), result);
-                    gameService.updateGameResult(List.of(gameUpdateRequest));
-                    bettingService.updateBettingResult(gameUpdateRequest);
-                }
-        );
+    @GetMapping("/version")
+    public String getVersion() {
+        return "v0.1";
     }
 }
