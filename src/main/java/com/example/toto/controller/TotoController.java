5b93dcaa58010f4fdbcc0f5fb91c7ae7b9284242
Sujin1517 <11tnwls12@gmail.com>
2024-06-25 17:14:33 +0900

    [fix]: Remove unused import(#34)

--- src/main/java/com/example/toto/controller/TotoController.java
+++ src/main/java/com/example/toto/controller/TotoController.java
@@ -1,18 +1,8 @@
 package com.example.toto.controller;
 
-import com.example.toto.domain.dto.request.GameRequest;
-import com.example.toto.domain.dto.request.GameUpdateRequest;
-import com.example.toto.domain.entity.Game;
-import com.example.toto.domain.repository.GameRepository;
-import com.example.toto.service.BettingService;
-import com.example.toto.service.GameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
-import java.util.*;
 
 @EnableScheduling
 @RestController
