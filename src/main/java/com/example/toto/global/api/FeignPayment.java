64a356025c5a25690928145720b5236c1aa93cd9
Sujin1517 <11tnwls12@gmail.com>
2024-06-28 10:21:16 +0900

    [fix]: Fix type and request method(#34)

--- src/main/java/com/example/toto/global/api/FeignPayment.java
+++ src/main/java/com/example/toto/global/api/FeignPayment.java
@@ -4,27 +4,25 @@ import com.example.toto.global.dto.request.UserPaymentRequest;
 import com.example.toto.global.dto.request.UserWinRequest;
 import com.example.toto.global.dto.response.UserPaymentResponse;
 import com.example.toto.global.dto.response.UserPointResponse;
+import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-@FeignClient(name = "PaymentApi", url = "http://192.168.0.10:8080")
+@FeignClient(name = "PaymentApi", url = "http://192.168.0.10:8083")
 public interface FeignPayment {
 
     @GetMapping("/api/v1/accounts/{userId}")
-    UserPointResponse getPointByUserId(@PathVariable("userId") String userId);
+    UserPointResponse getPointByUserId(@PathVariable("userId") UUID userId);
 
-    @PostMapping("/api/v1/payments/{userId}")
+    @PutMapping("/api/v1/payments/{userId}")
     UserPaymentResponse payTotoByUser(
             @PathVariable("userId") UUID userId,
             @RequestBody UserPaymentRequest req
     );
 
-    @PostMapping("/api/v1/win/{userId}")
+    @PostMapping("/api/v1/win/toto/{userId}")
     void sendWinUser(
             @PathVariable("userId") UUID userId,
             @RequestBody UserWinRequest req
