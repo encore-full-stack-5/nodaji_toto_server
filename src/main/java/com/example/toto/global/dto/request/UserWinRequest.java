a1e4cb11e2b2a54d1b787ad10b2f7a3403ad0aec
Sujin1517 <11tnwls12@gmail.com>
2024-06-28 10:21:50 +0900

    [fix]: Fix pay win dto(#34)

--- src/main/java/com/example/toto/global/dto/request/UserWinRequest.java
+++ src/main/java/com/example/toto/global/dto/request/UserWinRequest.java
@@ -1,14 +1,10 @@
 package com.example.toto.global.dto.request;
 
 public record UserWinRequest(
-        String type,
-        Integer leftMonths,
         Integer amount
 ) {
     public static UserWinRequest from(Integer amount){
         return new UserWinRequest(
-                "토토",
-                0,
                 amount
         );
     }
